43d382565e4b90b1dcb009912c430e2ceec3e52d
Imhotep Benjamin <imhotepbenjamin@gmail.com>
2023-12-09 13:41:57 -0500

    new commit

--- popup.js
+++ popup.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', function () {
   const modal = document.querySelector('.modal');
   const moreButton = document.querySelector('.more-notes');
   const exitModal = document.querySelector('.modal-exit, .exit-container');
+  const themesButton = document.querySelector('themes-button');
+  const fontsButton = document.querySelector('fonts-button');
+  const helpButton = document.querySelector('help-button');
+  const aboutMeButton = document.querySelector('about-me-button');
+
   moreButton.addEventListener('click', () => {
     modal.style.display = "flex";
   });
@@ -9,6 +14,8 @@ document.addEventListener('DOMContentLoaded', function () {
   exitModal.addEventListener('click', () => {
     modal.style.display = 'none';
   });
+
+  
 
 });
 
